a381ab3b332981dfe5ea2bde61c2ecc65b46e338
Nikolai_Kushner <n.kushner@imedia.by>
2019-03-06 13:19:55 +0300

    feat: solution -> 100 passing

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let triangles = 0,
+        length = preferences.length;
+
+    for (let i = 0; i < length; i++) {
+        const spichonee1 = preferences[i],
+            spichonee2 = preferences[spichonee1-1],
+            spichonee3 = preferences[spichonee2-1];
+
+        if (spichonee3 === i+1 && spichonee1 !== spichonee2) {
+            triangles++;
+            preferences[i] = null;
+        }
+    }
+    return triangles;
 };
